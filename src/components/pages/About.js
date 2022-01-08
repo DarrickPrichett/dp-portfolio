39934fe6b78d953d80987cf87498a9f8995c3d80
DarrickPrichett <darrickprichett@gmail.com>
2022-01-07 21:06:55 -0600

    added information

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,10 @@ const About = () => (
   <div>
     <h1>About Page</h1>
     <p>
-    Hello, I am a full stack web developer. I enjoy participating in the design and creation of scalable software. I have experience writing clean, functional code on both the front-end and back-end of a project. Testing and fixing bugs or other coding issues is another passion of mine.
+    Hello, My name is Darrick Prichett and I am a full stack web developer. I enjoy participating in the design and creation of scalable software. I have experience writing clean, functional code on both the front-end and back-end of a project.  
+    I utilize both client-side and server-side design architecture. Building the front-end of applications through appealing visual design. Developing and managing well-functioning databases and applications. I write effective Web APIs including Third-Party APIs as well as  Server-Side APIs. 
+    Testing and fixing bugs or other coding issues is another passion of mine. Testing software to ensure responsiveness and efficiency, troubleshoot, debug and upgrade software.
+    Creating security and data protection settings. Building features and applications with a mobile responsive design.
     </p>
   </div>
 );
